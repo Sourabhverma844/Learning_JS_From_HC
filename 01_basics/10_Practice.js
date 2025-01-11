756a9d82e6ef1fc869387c2cbada3b2909a39bcf
Sourabh Verma <sourabhverma844@gmail.com>
2025-01-11 21:50:04 +0530

    update

---
+++ 01_basics/10_Practice.js
@@ -0,0 +1,12 @@
+const accounId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 5 //not allowed because accountId is const
+accountEmail = "aniket.google"
+accountPassword = "212121"
+accountCity = "Bengluru"
+
+console.log(accounId);
+console.table([accountEmail,accountPassword,accountCity]);
