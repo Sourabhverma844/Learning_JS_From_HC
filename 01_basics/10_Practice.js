69133c5c8fb389d0487b95c159cbf3504a9d86ac
Sourabh Verma <sourabhverma844@gmail.com>
2025-01-12 10:23:36 +0530

    update

--- 01_basics/10_Practice.js
+++ 01_basics/10_Practice.js
@@ -14,4 +14,19 @@ console.table([accountEmail,accountPassword,accountCity]);
 "use strict"; // treat all JS code as newer version
 // but already hamara poora code newer version me hi run hota he
 
-alert(3+3)
+// alert(3+3) here we cant run this because we use nodejs, not browser
+
+let name = "Sourabh"
+let age = 24
+let isLoggedIn = false 
+console.log(typeof null); // null gives us object in output
+console.log(typeof undefined); // undefined gives us undefined in output
+console.log(typeof "Sourabh") // string
+
+let score1 = 33
+console.log(typeof score1)
+let score2 = "33"
+console.log(typeof score2)
+
+let valueInNumber = Number(score2)
+console.log(typeof score2)
