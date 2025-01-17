4461d28b1e4604208c72a18fad599db8dc27f208
Sourabh Verma <sourabhverma844@gmail.com>
2025-01-17 23:44:14 +0530

    Update

--- 01_basics/10_Practice.js
+++ 01_basics/10_Practice.js
@@ -18,7 +18,7 @@ console.table([accountEmail,accountPassword,accountCity]);
 
 let name = "Sourabh"
 let age = 24
-let isLoggedIn = false 
+let isLoggedIn1 = false 
 console.log(typeof null); // null gives us object in output
 console.log(typeof undefined); // undefined gives us undefined in output
 console.log(typeof "Sourabh") // string
@@ -38,3 +38,52 @@ console.log(typeof valueInNumber);
 true => 1
 false => 0
 */
+
+let isLoggedIn2 = undefined
+let Inboolean = Boolean(isLoggedIn2)
+console.log(Inboolean);
+console.log(isLoggedIn2);
+
+let isLoggedIn3 = null
+let Inboolean2 = Boolean(isLoggedIn3)
+console.log(typeof Inboolean2)
+console.log(Inboolean2)
+console.log(typeof isLoggedIn3)
+
+console.log(1 + "2" + "2")
+
+console.log(+true);
+console.log(+"");
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0)
+
+console.log(undefined = 0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined === 0) 
+
+let userEmail;
+console.log(userEmail);
+console.log(typeof userEmail);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 456574337592795368675n
+
+const heros = ["shaktiman", "nagraj", "doga"]
+let myObje = {
+    name: "hitesh",
+    age: 22,
+}
+const myfunction = function(){
+    console.log("Hello World")
+}
