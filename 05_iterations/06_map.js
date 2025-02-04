23dd0e66e2533154a328c656eb834fdc091a7c5f
Sourabh Verma <sourabhverma844@gmail.com>
2025-02-04 22:44:11 +0530

    update

--- 05_iterations/06_map.js
+++ 05_iterations/06_map.js
@@ -12,8 +12,5 @@ console.log(newno2);
 console.log('"------------------------------------------------------------------"')
 // how chainning works.
 // In chaining, we can use two or three methods together.
-const newno3 = MyNumbers
-                        .map( (num) => num * 2 )
-                        .map( (num) => num + 1 )
-                        .filter( (num) => num >= 40 )
-console.log(newno3)
+const newno3 = MyNumbers.map( (num) => num * 2 ).map( (num) => num + 1 ).filter( (num) => num >= 40 )
+console.log(newno3);
--- 05_iterations/07_reduce.js
+++ 05_iterations/07_reduce.js
@@ -7,27 +7,27 @@ the final result of running the reducer acros  all elements of the array is a si
 
 const myNums = [1, 2, 3]
 
-const myTotal1 = myNums.reduce(function (accumulator, currentValue) {
-    console.log(`acc: ${accumulator} and curval: ${currentValue}`);
-    return accumulator + currentValue // yaha jo value return hui he 0 + 1 = 1 ab ye jaegi accumulator me 
+const myTotal1 = myNums.reduce(function (value_in_accumulator, currentValue) {
+    console.log(`acc: ${value_in_accumulator} and curval: ${currentValue}`);
+    return value_in_accumulator + currentValue // yaha jo value return hui he 0 + 1 = 1 ab ye jaegi accumulator me 
 }, 0) // yaha jo 0 he wo accumulaor ki initial value he.
 console.log(myTotal1);
 
-const myTotal2 = myNums.reduce( (accumulator,currentValue) => accumulator+currentValue,10)
+const myTotal2 = myNums.reduce( (accm,curVal) => accm+curVal,10)
 console.log(myTotal2)
 
 const shoppingCart = [
     {
-        itemName : "Sunscreen",
-        Price : 500
+        itemName : "DermaCo_Sunscreen",
+        Price : 750
     },
     {
-        itemName : "Facewash",
-        Price : 250
-    },
-    {
-        itemName : "Nightcream",
+        itemName : "Salicylic_Acid_Facewash",
         Price : 400
+    },
+    {
+        itemName : "Ratinol_Nightcream",
+        Price : 500
     },
     {
         itemName : "Undereyecream",
@@ -35,4 +35,5 @@ const shoppingCart = [
     },
 ]
 const TotalPrice = shoppingCart.reduce( (acc, item) => (acc + item.Price), 0)
-console.log(TotalPrice)
+console.log(`Your Total is ${TotalPrice} and Tax is ${((TotalPrice*18.5)/100)}`)
+console.log(`Your Have To Pay ${TotalPrice+((TotalPrice*18.5)/100)} rs Only.`)
